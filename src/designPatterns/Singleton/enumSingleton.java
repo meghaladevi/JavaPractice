11f09971ba0cebd8120a490e1b96531bd7f0072a
Meghala Devi <37797970+meghaladevi@users.noreply.github.com>
2019-07-16 16:07:54 +0530

    Committing

--- src/designPatterns/Singleton/enumSingleton.java
+++ src/designPatterns/Singleton/enumSingleton.java
@@ -4,9 +4,7 @@ package designPatterns.Singleton;
  * Created by shanmm5 on 16-07-2019.
  */
 public enum enumSingleton {
-
     INSTANCE;
-
     public String getConfiguration() {
         return  "XX";
     }
