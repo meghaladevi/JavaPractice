11f23294bacab22f6b58f9c30bef8416cf648c17
Meghala Devi <37797970+meghaladevi@users.noreply.github.com>
2019-07-16 14:58:56 +0530

    Add files via upload

---
+++ enumSingleton.java
@@ -0,0 +1,13 @@
+package designPatterns.Singleton;
+
+/**
+ * Created by shanmm5 on 16-07-2019.
+ */
+public enum enumSingleton {
+
+    INSTANCE;
+
+    public String getConfiguration() {
+        return  "XX";
+    }
+}
